4b0adb318270b029581106ad9c0288e71d74dca1
Ankita <50478681+Ahana001@users.noreply.github.com>
2022-06-18 09:25:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+var readlineSync = require('readline-sync');
+var chalk = require('chalk');
+var log = console.log;
+var right = chalk.bgGreenBright.black;
+var wrong = chalk.bgRedBright.black;
+var score = 0;
+const highScores = [
+    {
+        name: "Ankita",
+        score: "5",
+    },
+    {
+        name: "John",
+        score: "4",
+    },
+];
+var user = readlineSync.question("What is your name? ");
+log("Welcome ",user , "to Do YOU KNOW ANKITA QUIZ ? ");
+
+function play(question,answer){
+  var userAns = readlineSync.question(question);
+  if(userAns.toLocaleLowerCase() === answer){
+    log(right("Yay, You are right :)"));
+    score = score + 1;
+  }else{
+    log(wrong("Yay, You are  wrong"));
+  }
+}
 
 var questions = [
   {
@@ -11,6 +39,13 @@ var questions = [
   {
     question : "My favourite outdoor game?  \n",
     answer : "badminton"
+  },{
+    question : "what is my favourite movie? \n",
+    answer : "vivah"
+  },
+  {
+  question : "What kind of clothes do I not like to wear? \n",
+  answer : "jeans"
   }
 ] 
 
